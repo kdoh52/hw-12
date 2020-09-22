43f3b6374f2ed3141c73e26157bda95807cb59a6
kdoh52 <kdoh52@gmail.com>
2020-09-22 16:58:40 -0700

    all features functional

--- employeeTracker.js
+++ employeeTracker.js
@@ -26,7 +26,7 @@ function runSearch() {
       choices: [
         "View All Employees",
         "View All Employees By Department",
-        "View All Employeees By Manager",
+        "View All Employees By Manager",
         "Add Employee",
         "Remove Employee",
         "Update Employee Role",
@@ -44,12 +44,14 @@ function runSearch() {
         viewByDepartment();
         break;
 
-      case "View All Employeees By Manager":
+      case "View All Employees By Manager":
         viewByManager();
+        // breaks when manager name is invalid
         break;
 
       case "Add Employee":
         addEmployee();
+        // breaks when manager name is invalid
         break;
 
       case "Remove Employee":
@@ -62,6 +64,7 @@ function runSearch() {
     
       case "Update Employee Manager":
         updateManager();
+        // breaks when manager name is invalid
         break;
 
       case "exit":
@@ -146,6 +149,124 @@ function viewByDepartment() {
         console.table(employeeArray);
         runSearch();
       })
+    })
+}
+
+
+function viewByManager() {
+  inquirer
+    .prompt([
+      {
+        name: "managerFirst",
+        type: "input",
+        message: "Manager's first name?"
+      },
+      {
+        name: "managerLast",
+        type: "input",
+        message: "Manager's last name?"
+      }
+    ])
+    .then(function(answer) {
+      let managerID
+      let managerQuery = "SELECT employee.id FROM employee WHERE first_name=? AND last_name=?"
+      connection.query(managerQuery, [answer.managerFirst, answer.managerLast], function(err, res) {
+        managerID = res[0].id;
+        // console.log(managerID)
+
+        var query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name, role.salary, e.manager_id, CONCAT(m.first_name, ' ', m.last_name) AS manager_name ";
+        query += "FROM employee e LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id INNER JOIN employee m ON m.id = e.manager_id WHERE e.manager_id=? GROUP BY e.id";
+        connection.query(query, [managerID], function(err, res) {
+          if (err) throw err;
+          // console.log(res);
+          var employeeArray = [];
+          for (var i = 0; i < res.length; i++) {
+            var obj = {
+              id: res[i].id,
+              first_name: res[i].first_name,
+              last_name: res[i].last_name,
+              title: res[i].title,
+              department: res[i].name,
+              salary: res[i].salary,
+              manager: res[i].manager_name
+            }
+            employeeArray.push(obj)
+          }
+          console.table(employeeArray);
+          runSearch();
+        })
+      })
+    })
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "firstName",
+        type: "input",
+        message: "Employee's first name?"
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "Employee's last name?"
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "Employee's role?",
+        choices: [
+          "Regional Manager",
+          "Assistant Regional Manager",
+          "Salesperson",
+          "Sales Representative",
+          "Accountant"
+        ]
+      },
+      {
+        name: "managerFirst",
+        type: "input",
+        message: "Manager's first name?"
+      },
+      {
+        name: "managerLast",
+        type: "input",
+        message: "Manager's last name?"
+      }
+    ])
+    .then(function(answer) {
+      let roleID
+      if (answer.role === "Regional Manager") {
+        roleID = 1;
+      } else if (answer.role === "Assistant Regional Manager") {
+        roleID = 2;
+      } else if (answer.role === "Salesperson") {
+        roleID = 3;
+      } else if (answer.role === "Sales Representative") {
+        roleID = 4;
+      } else if (answer.role === "Accountant") {
+        roleID = 5;
+      }
+      
+      let managerID
+      let managerQuery = "SELECT employee.id FROM employee WHERE first_name=? AND last_name=?"
+      connection.query(managerQuery, [answer.managerFirst, answer.managerLast], function(err, res) {
+        if (err) {runSearch()};
+        managerID = res[0].id;
+        // console.log(answer.firstName);
+        // console.log(answer.lastName);
+        // console.log(roleID);
+        // console.log(managerID);
+
+        let query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+        connection.query(query, [answer.firstName, answer.lastName, roleID, managerID], function(err, res) {
+          // console.log("updated lol");
+          if (err) {runSearch()};
+          runSearch();
+        })
+      })
+
     })
 }
 
@@ -249,131 +370,17 @@ function updateManager() {
       let managerID
       let managerQuery = "SELECT employee.id FROM employee WHERE first_name=? AND last_name=?"
       connection.query(managerQuery, [answer.managerFirst, answer.managerLast], function(err, res) {
+        if (err) {runSearch()};
         managerID = res[0].id;
-        console.log(managerID)
+        // console.log(managerID);
 
         let query = "UPDATE employee SET manager_id=? WHERE first_name=? AND last_name=?";
         connection.query(query, [managerID, answer.firstName, answer.lastName], function(err, res) {
           // console.log("updated lol");
+          if (err) {runSearch()};
           runSearch();
         })
       })
 
     })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function artistSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-        }
-        runSearch();
-      });
-    });
-}
-
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
-  });
-}
-
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
-
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        if (err) throw err;
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
-    });
 }
